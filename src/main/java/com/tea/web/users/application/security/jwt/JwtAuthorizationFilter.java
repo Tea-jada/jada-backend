1e37bf0a5e12697b648ba59371f1141fc132051a
kwj0605 <rladnwlsok@naver.com>
2025-05-14 00:32:18 +0900

    [feat] RefreshToken 추가 #1

--- src/main/java/com/tea/web/users/application/security/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/tea/web/users/application/security/jwt/JwtAuthorizationFilter.java
@@ -32,20 +32,36 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String tokenValue = jwtUtil.getJwtFromHeader(req);
-        if (StringUtils.hasText(tokenValue)) {
-            log.info(tokenValue);
-            Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
+        String accessValue = jwtUtil.getJwtFromHeader(req, JwtUtil.AUTHORIZATION_HEADER);
+        String refreshValue = jwtUtil.getJwtFromHeader(req, JwtUtil.REFRESH_TOKEN_HEADER);
+
+        if (StringUtils.hasText(refreshValue)) {
+            if (!jwtUtil.validateRefreshToken(refreshValue)) {
+                log.error("RefreshToken Error");
+                return;
+            }
+        }
+        else if (StringUtils.hasText(accessValue)) {
+            if (!jwtUtil.validateToken(accessValue)) {
+                log.error("AccessToken Error");
+                return;
+            }
+            Claims info = jwtUtil.getUserInfoFromToken(accessValue);
+
             try {
                 setAuthentication(info.getSubject());
             } catch (Exception e) {
                 log.error(e.getMessage());
+                res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                res.setCharacterEncoding("utf-8");
+                res.getWriter().write("상태 : " + res.getStatus() + e.getMessage());
                 return;
             }
         }
         filterChain.doFilter(req, res);
     }
 
+    // 인증 처리
     public void setAuthentication(String email) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         Authentication authentication = createAuthentication(email);
@@ -54,6 +70,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         SecurityContextHolder.setContext(context);
     }
 
+    // 인증 객체 생성
     private Authentication createAuthentication(String email) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
--- src/main/java/com/tea/web/users/application/security/jwt/JwtUtil.java
+++ src/main/java/com/tea/web/users/application/security/jwt/JwtUtil.java
@@ -1,9 +1,8 @@
 package com.tea.web.users.application.security.jwt;
 
 import com.tea.web.users.domain.model.Role;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.tea.web.users.infrastructure.dao.RedisDao;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.crypto.SecretKey;
+import java.time.Duration;
 import java.util.Date;
 
 @Component
@@ -20,48 +20,119 @@ import java.util.Date;
 public class JwtUtil {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
     private static final String BEARER_PREFIX = "Bearer ";
 
-    private final String issuer;
-    private final long EXPIRATION_TIME;
+    private final RedisDao redisDao; // RefreshToken 저장을 위해 Redis 사용
+    private final String issuer; // 토큰 생성 서비스 이름
     private final SecretKey secretKey;
+
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 24시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 3; // 3일
 
     public JwtUtil(
             @Value("${service.jwt.secret-key}") String secretKeyValue,
             @Value("${spring.application.name}") String issuer,
-            @Value("${service.jwt.access-expiration}") long EXPIRATION_TIME
+            RedisDao redisDao
     ) {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKeyValue));
         this.issuer = issuer;
-        this.EXPIRATION_TIME = EXPIRATION_TIME;
+        this.redisDao = redisDao;
     }
 
     // 토큰 생성
-    public String createToken(Long userId, String email, String username, Role role) {
+    public String createToken(Long userId, String username, String nickname, Role role) {
         return BEARER_PREFIX + Jwts.builder()
                 .subject(username)
                 .issuer(issuer)
                 .claim("userId", userId)
-                .claim("email", email)
                 .claim("username", username)
+                .claim("nickname", nickname)
                 .claim("role", role)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRE_TIME))
                 .signWith(secretKey, SignatureAlgorithm.HS512)
                 .compact();
     }
 
-    //토큰 가져오기 헤더
-    public String getJwtFromHeader(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    // refresh token 생성
+    public String createRefreshToken(String username) {
+        String refreshToken = BEARER_PREFIX + Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRE_TIME))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+        // Redis에 RefreshToken 넣기
+        // "REFRESH_TOKEN_EXPIRE_TIME"만큼 시간이 지나면 삭제됨
+        redisDao.setValues(username, refreshToken, Duration.ofMillis(REFRESH_TOKEN_EXPIRE_TIME));
+
+        return refreshToken;
+    }
+
+    // 헤더에서 JWT 토큰 가져오기
+    public String getJwtFromHeader(HttpServletRequest request, String tokenHeader) {
+        String bearerToken = request.getHeader(tokenHeader);
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(BEARER_PREFIX.length()).trim();
         }
         return null;
     }
 
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
     // 토큰에서 사용자 정보 가져오기
     public Claims getUserInfoFromToken(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+    }
+
+    // RefreshToken 검증
+    public boolean validateRefreshToken(String token) {
+        // 기본적인 JWT 검증
+        if (!validateToken(token)) return false;
+
+        try {
+            // token에서 username 추출하기
+            String username = getUserNameFromToken(token);
+            // Redis에 저장된 RefreshToken과 비교하기
+            String redisToken = (String) redisDao.getValues(username);
+            return token.equals(redisToken);
+        } catch (Exception e) {
+            log.info("RefreshToken Validation Failed", e);
+            return false;
+        }
+    }
+
+    // 토큰에서 username 추출
+    public String getUserNameFromToken(String token) {
+        try {
+            // 토큰 파싱해서 클레임 얻기
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // 사용자 이름(subject) 반환
+            return claims.getSubject();
+        } catch (ExpiredJwtException e) {
+            // 토큰이 만료되어도 클레임 내용을 가져올 수 있음
+            return e.getClaims().getSubject();
+        }
     }
 }
